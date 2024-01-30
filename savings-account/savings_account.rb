394fe67f146ba3207afbb076f45d614f986404dd
Anuj Varma <anujvarma@thumbtack.com>
2024-01-29 21:25:35 -0800

    Savings account complete

--- savings-account/savings_account.rb
+++ savings-account/savings_account.rb
@@ -1,30 +1,35 @@
 module SavingsAccount
-
-  # interest_rate: 0.5% for balance < 1000
-  # interest_rate: 1.621% for 1000 <= balance < 5000
-  # interest_rate: 2.475% for balance >= 5000
-  # interest_rate: 3.213% for balance < 0
+  # - 0.5% for a non-negative balance less than `1000` dollars.
+  # - 1.621% for a positive balance greater than or equal to `1000` dollars and less than `5000` dollars.
+  # - 2.475% for a positive balance greater than or equal to `5000` dollars.
+  # - 3.213% for a negative balance (results in negative interest).
   def self.interest_rate(balance)
     case balance
-      when 0...1000
-        0.5
-      when 1000...5000
-        1.621
-      when 5000..Float::INFINITY
-        2.475
-      else
-        3.213
-      end
+    when 0...1000
+      0.5
+    when 1000...5000
+      1.621
+    when 5000..Float::INFINITY
+      2.475
+    else
+      3.213
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    balance * (1 + interest_rate(balance) / 100)
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    if current_balance.negative?
+      Float::INFINITY
+    else
+      years = 0
+      while current_balance < desired_balance
+        current_balance = annual_balance_update(current_balance)
+        years += 1
+      end
+      years
+    end
   end
 end
-
-s = SavingsAccount.interest_rate(1000)
-puts "s: #{s}"
