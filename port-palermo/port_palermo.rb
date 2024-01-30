3a5bf0a323712e9902be227dea9406d557441b11
Anuj Varma <anujvarma@thumbtack.com>
2024-01-29 23:07:38 -0800

    Port Palermo is done

--- port-palermo/port_palermo.rb
+++ port-palermo/port_palermo.rb
@@ -1,11 +1,17 @@
 module Port
   # TODO: define the 'IDENTIFIER' constant
+  IDENTIFIER = :PALE
 
   def self.get_identifier(city)
-    raise 'Please implement the Port.get_identifier method'
+    city[0..3].upcase.to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    raise 'Please implement the Port.get_terminal method'
+    case ship_identifier[0..2].upcase.to_sym
+    when :OIL, :GAS
+      :A
+    else
+      :B
+    end
   end
 end
